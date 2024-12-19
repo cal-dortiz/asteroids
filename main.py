30e832301a9c7a19779183d6ad76ebcff99ad290
cal-dortiz <djortiz@berkeley.edu>
2024-12-18 19:39:11 -0600

    Added player object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -11,11 +12,16 @@ def main():
     # Set GUI
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Instantiat Player
+    player = Player(x = SCREEN_WIDTH / 2,
+                    y = SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
