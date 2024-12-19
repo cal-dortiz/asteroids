0e28c597f4594964e8bec93dee4d508c46d41519
cal-dortiz <djortiz@berkeley.edu>
2024-12-18 19:47:02 -0600

    Add left/right dir

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
-from circleshape import CircleShape
-from constants import PLAYER_RADIUS
 import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+
 
 class Player(CircleShape):
     def __init__(self, x:int,y:int):
@@ -20,4 +21,14 @@ class Player(CircleShape):
                             'white', 
                             self.triangle(), 
                             2)
-        pass
+    
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
